577ce5ab06abdbf4d9a6a7f913c9dfde28913617
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2026-03-20 00:03:17 +0530

    changes updated v1

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ docsearch = PineconeVectorStore.from_existing_index(
 
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
-chatModel = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+chatModel = ChatGoogleGenerativeAI(model="models/gemini-1.5-flash")
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
